e50521aaa73dba0a64837a70ec387ff67321be90
tomasz.zbroszczyk <tomekzbroszczyk@gmail.com>
2021-02-01 14:32:25 +0100

    Added product prices

--- app/src/main/java/com/example/shopapk/Data/Data.java
+++ app/src/main/java/com/example/shopapk/Data/Data.java
@@ -3,6 +3,8 @@ package com.example.shopapk.Data;
 import com.example.shopapk.Classes.User;
 import com.here.android.mpa.common.GeoCoordinate;
 
+import java.util.Random;
+
 public class Data{
     public static double lat = 0.0;
     public static double lon = 0.0;
@@ -15,11 +17,29 @@ public class Data{
     public static boolean check_log = false;
     public static boolean darkModeCheck = false;
     public static boolean is_Dark_Mode_On = false;
+    public static int leftLimit = 48;
+    public static int rightLimit = 122;
+    public static int targetStringLength = 32;
 
 
     public static boolean isValidEmail(String email) {
         String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
         return email.matches(regex);
+    }
+
+    public static String randomString() {
+
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
+
+        System.out.println(generatedString);
+        return generatedString;
     }
 
 }
